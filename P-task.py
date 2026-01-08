05605f24eabe17e57641577a19490533f5175d5f
GT Moukim <lamraouimoukim@gmail.com>
2026-01-08 02:39:16 +0100

    code work 100%

--- P-task.py
+++ P-task.py
@@ -1,26 +1,32 @@
 print("---welcome to GT task manager---")
 tasks=[]
-i= 0
 while True:
-    num=int(input("\n choose ur number (1-4):\n 1.add task\n 2.view tasks\n 3.delete task\n 4.exit \n enter ur choice: "))
-    print("1.add task\n 2.view tasks \n 3.delete task \n 4.exit")
-    if num==1:
+    i=0
+    num=input("\n choose ur number (1-4):\n 1.add task\n 2.view tasks\n 3.delete task\n 4.exit \n enter ur choice: ")
+    if num=="1":
         new_task = input("enter task name: ")
         tasks.append(new_task)
-    print("\n")
-    if num==2:
+        print("task add ")
+    if num=="2":
         print("---your tasks are:")
         for t in tasks:
             if t!="":
                 i+=1
                 print(f"{i}. {t}")
-                print("\n this all tasks")
-    if num==3:
-        num=int(input("enter task number to delete: "))
-        print(f"{i}. {t}")
-        num+=1
-        tasks.pop(num-1)
+        print("\n this all tasks")
+    if num=="3":
+        i=0
+        for t in tasks:
+            if t!="":
+                i+=1
+                print(f"{i}. {t}")
+        numdel=int(input("enter task number to delete: "))
+        numdel-=1
+        tasks.pop(numdel)
         print("complet delete !!")
-    if num==4:
+    if num=="4":
         print("exiting task manager")
         break
+    elif num!="1" and num!="2" and num!="3" and num!="4" :
+        print("sorry you can just choose number (1-4)")
+ 
--- test.py
+++ test.py
@@ -1,91 +1,48 @@
-task = []
+tasks = []
 
 while True:
+    print("\n--- TO DO LIST MENU ---")
+    print("1: Add task")
+    print("2: View tasks")
+    print("3: Delete task")
+    print("4: Exit")
 
-print("\n--- TO DO LIST MENU ---")
+    choice = input("Select an option (1-4): ")
 
-print("1: Add task")
+    if choice == "1":
+        # Add new task
+        new_task = input("Enter your task: ")
+        tasks.append(new_task)
+        print("Task added successfully.")
 
-print("2: View task")
+    elif choice == "2":
+        # View tasks
+        if not tasks:
+            print("Your task list is empty.")
+        else:
+            print("\nYour current tasks:")
+            for index, task in enumerate(tasks, 1):
+                print(f"{index}. {task}")
 
-print("3: Delete task")
+    elif choice == "3":
+        # Delete a task
+        if not tasks:
+            print("Nothing to delete.")
+        else:
+            print("\nChoose the number of the task to delete:")
+            for index, task in enumerate(tasks, 1):
+                print(f"{index}. {task}")
+            try:
+                task_num = int(input("Task number: "))
+                removed = tasks.pop(task_num - 1)
+                print(f"Deleted: '{removed}'")
+            except (ValueError, IndexError):
+                print("Invalid number.")
 
-print("4: Exist")
-
-
-
-choice = input("Select a option (1-4):")
-
-
-
-if choice == "1":
-
-    # Add new task
-
-    new_task = input("Enter your task: ")
-
-    task.append(new_task)
-
-    print("Task added sucessfully")
-
-
-
-elif choice == "2":
-
-    # View task
-
-    if not task:
-
-        print("list empty")
-
-    else:
-
-        print("\nYour current Task")
-
-        for index, task in enumerate:
-
-            print(f"{index}. {task}")
-
-elif choice == "3":
-
-    # View task
-
-    if not task:
-
-        print("nothing to delete")
+    elif choice == "4":
+        # Exit the program
+        print("Exiting...")
+        break
 
     else:
-
-        print("\nChoose number of the task to delete: ")
-
-        for index, task in enumerate(task, 1):
-
-            print(f"{index}. {task}")
-
-        try:
-
-            task_num = int(input("Task number: "))
-
-            removed = task.pop(task_num - 1)
-
-            print(f"Deleted: '{removed}'")
-
-        except (ValueError, IndexError):
-
-            print("Invalid number")
-
-
-
-elif choice == "4":
-
-    # Exit or stop script
-
-    print("Exiting...")
-
-    break
-
-
-
-else:
-
-    print("Invalid choice")``
+        print("Invalid choice. Try again.")
